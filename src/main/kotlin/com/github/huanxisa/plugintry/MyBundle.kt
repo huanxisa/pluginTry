b2a024cd897e66eec97063b8a65208aac0fa5309
GitHub Action <action@github.com>
2021-09-09 09:46:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/huanxisa/plugintry/MyBundle.kt
+++ src/main/kotlin/com/github/huanxisa/plugintry/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.huanxisa.plugintry
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/huanxisa/plugintry/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/huanxisa/plugintry/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.huanxisa.plugintry.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.huanxisa.plugintry.MyBundle
 
 class MyApplicationService {
 
